27f50bc38649a9bb695e49670fc5f5bf11dc7f0a
Israel David <israeldiv100@gmail.com>
2026-01-16 16:38:31 -0300

    minor changes

--- src/main/java/com/israel/expensemanager/dtos/UpdateDTO.java
+++ src/main/java/com/israel/expensemanager/dtos/UpdateDTO.java
@@ -1,10 +1,8 @@
 package com.israel.expensemanager.dtos;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 public record UpdateDTO(@NotBlank String expense,
                         @NotBlank BigDecimal price) {
