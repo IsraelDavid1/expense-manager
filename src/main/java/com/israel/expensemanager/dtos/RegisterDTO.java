50d8afaf16b356cb7a48022117587cd90f790fa5
Israel David <israeldiv100@gmail.com>
2026-01-23 11:29:10 -0300

    minor fixies

--- src/main/java/com/israel/expensemanager/dtos/RegisterDTO.java
+++ src/main/java/com/israel/expensemanager/dtos/RegisterDTO.java
@@ -1,8 +1,6 @@
 package com.israel.expensemanager.dtos;
 
-import com.israel.expensemanager.models.UserRole;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 public record RegisterDTO(@NotBlank String username,
